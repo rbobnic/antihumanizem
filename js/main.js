b08739c1d3591b375cd43ccfc767d5272930b69f
rbobnic <114941884+rbobnic@users.noreply.github.com>
2024-11-18 08:19:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,10 @@
-//slider
-function moveSlides(sliderId, n) {
-    const slides = document.querySelectorAll(`#${sliderId} .slide`);
-    let currentSlide = Array.from(slides).findIndex(slide => slide.style.display === 'block');
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
 
-    // If no active slide found (default), start from the first slide
-    if (currentSlide === -1) {
-        currentSlide = 0;
-    }
-
-    slides[currentSlide].style.display = 'none';
-    currentSlide += n;
-
-    if (currentSlide >= slides.length) {
-        currentSlide = 0; // Wrap back to first slide
-    } else if (currentSlide < 0) {
-        currentSlide = slides.length - 1; // Wrap to the last slide
-    }
-
-    slides[currentSlide].style.display = 'block';
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    const sliders = document.querySelectorAll('.slider');
-    sliders.forEach(slider => {
-        const slides = slider.querySelectorAll('.slide');
-        slides[0].style.display = 'block'; // Display first slide in each slider by default
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
     });
 });
 
